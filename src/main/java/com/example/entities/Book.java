9239fea571281a0721383571de86422b87410f7d
Anil <an.kumar3407@gmail.com>
2025-01-08 22:51:34 +0530

    newly added file
    Signed-off-by: Anil <an.kumar3407@gmail.com>

---
+++ src/main/java/com/example/entities/Book.java
@@ -0,0 +1,15 @@
+package com.example.entities;
+
+
+
+public class Book {
+
+    private Long id;
+    private String title;
+    private String author;
+    private double price;
+    private int stock;
+
+    // Getters and Setters
+}
+
